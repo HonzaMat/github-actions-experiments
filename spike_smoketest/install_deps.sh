34fe929bab745478e8ad196dae0a00a20d4cc641
Jan Matyas <info@janmatyas.net>
2026-07-26 20:08:39 +0200

    improve dependency installation

--- spike_smoketest/install_deps.sh
+++ spike_smoketest/install_deps.sh
@@ -2,18 +2,33 @@
 
 # SPDX-License-Identifier: CC0-1.0
 
-# Install runtime dependencies needed by Spike (currently
-# this is the device tree compiler only).
+# Install the dependencies needed to run Spike:
+# - device tree compiler
+# And dependencies for the run_smoketest.sh script:
+# - diff
 
 set -euo pipefail
 
-if command -v dnf >/dev/null 2>&1; then
-    # RockyLinux, AlmaLinux, Fedora
-    dnf install -y dtc
-elif command -v apt-get >/dev/null 2>&1; then
-    # Ubuntu, Debian
-    apt-get install -y device-tree-compiler
-else
-    echo "Don't know how to install spike dependencies on this system." >&2
+if [ "$#" -ne 1 ]; then
+    echo "Error: expected exactly one argument (name of the docker container)." >&2
     exit 1
 fi
+
+case "$1" in
+    "ubuntu:jammy"|"ubuntu:noble"|"ubuntu:resolute"|"debian:12"|"debian:13")
+        apt-get update
+        apt-get install -y device-tree-compiler
+        ;;
+    "fedora:44"|"rockylinux:9")
+        dnf install -y diff dtc
+        ;;
+    "rockylinux:8")
+        dnf install -y diff
+        dnf install --enablerepo=devel -y dtc
+        ;;
+    *)
+        echo "Don't know what dependencies to install on this system." >&2
+        exit 2
+esac
+
+exit 0
